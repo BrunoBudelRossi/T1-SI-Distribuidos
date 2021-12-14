b97d5e18c101a9c9647230884656d4918a4f8099
hierror <hierrodscherer@gmail.com>
2021-12-14 00:43:44 -0300

    finish

--- frontend/src/components/Task/index.js
+++ frontend/src/components/Task/index.js
@@ -3,18 +3,23 @@ import TrashCanIcon from "../Icons/TrashCanIcon";
 import "./Task.css";
 
 const Task = ({ id, description, done, updateTask, deleteTask, ...props }) => (
-    <li className="task" {...props}>
-        <input
-            className="task__checkbox"
-            type="checkbox"
-            checked={done}
-            onChange={() => updateTask(id)}
-        />
-        <p className="task__description">{description}</p>
-        <button className="task__button" onClick={() => deleteTask(id)}>
-            <TrashCanIcon />
-        </button>
-    </li>
+  <li className="task" {...props}>
+    <div className="task__checkbox-wrapper">
+      <input
+        id={id}
+        name="checkbox"
+        className="task__checkbox"
+        type="checkbox"
+        checked={done}
+        onChange={() => updateTask(id)}
+      />
+      <label htmlFor={id} className="task__checkbox-label"></label>
+    </div>
+    <p className="task__description">{description}</p>
+    <button className="task__button" onClick={() => deleteTask(id)}>
+      <TrashCanIcon />
+    </button>
+  </li>
 );
 
 export default Task;
